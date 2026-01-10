86aeb8585feb2b33b3dced91a17e57bd808b29ef
Pomid0rkaaa <artem.minecratt@gmail.com>
2026-01-11 00:43:09 +0200

    separate file for rewards

---
+++ js/data/rewards.js
@@ -0,0 +1,37 @@
+export const REWARDS = [
+    {
+        type: "stat",
+        id: "heal",
+        name: "Зелье",
+        desc: "+30 HP",
+        icon: "❤️",
+    },
+    {
+        type: "stat",
+        id: "maxhp",
+        name: "Выносливость",
+        desc: "+10 Max HP",
+        icon: "💪",
+    },
+    {
+        type: "stat",
+        id: "shield",
+        name: "Броня",
+        desc: "+20 щита",
+        icon: "🛡️",
+    },
+    {
+        type: "stat",
+        id: "gold",
+        name: "Кошель",
+        desc: "+40 золота",
+        icon: "🪙",
+    },
+    {
+        type: "stat",
+        id: "full",
+        name: "Полный отдых",
+        desc: "Полное лечение",
+        icon: "💖",
+    },
+]
